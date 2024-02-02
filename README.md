bfda61f5a335a291e4fcae4918ac447a9b42f9c1
Martin Wacker <martas@imm.cz>
2024-02-02 10:59:46 +0100

    added server example

--- README.md
+++ README.md
@@ -1,12 +1,45 @@
-Example addon - New directory structure
-========================================
+Example addon
+=============
+
+* New directory structure - the good one *
 
 
 Premises
 --------
 
-Name of the addon and its version is defined in `package.py` it is the single source of truth for the version information.
+Addon repository (at least its server part) should work when cloned in the addon directory.
 
+```bash
+cd addons/
+mkdir example
+cd example
+git clone https://github.com/ynput/ayon-example-rez-addon dev
+```
+
+
+Value of the version part of the addon path is not significant, so for development, it is possible to use `dev`.
+
+```
+addons/example
+  ├── 1.0.0
+  └── dev
+      ├── client
+      ├── frontend
+      │   ├── dist
+      │   └── package.json
+      ├── package
+      ├── package.py
+      ├── private
+      ├── public
+      ├── README.md
+      ├── server
+      │   ├── __init__.py
+      │   ├── settings.py
+      │   └── site_settings.py
+      └── services
+```
+
+Name of the addon and its version is defined in `package.py` it is the single source of truth for the version information.
 
 The addon build script (implementation TBD - rez build?) creates a zip package with similar structure as the repository itself,
 but should ommit files not needed by the server and buil
@@ -18,7 +51,7 @@ Directory structure
 ### client
 
 Source code of the client code. Build script (?) should create a client zip file from the source and put it to `private` directory,
-create `checksums.json` and include it to the zip file.
+create `checksums.json` and include it to the zip file. **TODO**
 
 ### frontend
 
@@ -26,7 +59,9 @@ Web frontend of the addon. Frontend is served if `frontend/dist/index.html` exis
 If the frontend is a javascript app, dist directory should be gitignored, build script should run `yarn build` (or equivalent)
 and put the `frontend/dist` to the resulting zip package. 
 
-Other files (`src/`, `package.json`, `node_modules`) should not be packed.
+Other files (`src/`, `package.json`, `node_modules`...) should not be packed.
+
+Frontend is no longer a subdirectory of "server".
 
 ### package
 
@@ -39,12 +74,14 @@ This directory should be gitignored.
 Contains static content available for download for authenticated users.
 
 If the addon does not use this functionality, the directory shouldn't be present.
+If the directory exists, build script packs it to the resulting zip file.
 
 ### public
 
 Contains static content available for download publicly.
 
 If the addon does not use this functionality, the directory shouldn't be present.
+If the directory exists, build script packs it to the resulting zip file.
 
 ### server
 
---
+++ server/settings.py
@@ -0,0 +1,363 @@
+from typing import Literal, TYPE_CHECKING
+
+from pydantic import Field, validator
+
+from ayon_server.lib.postgres import Postgres
+from ayon_server.settings import BaseSettingsModel, ensure_unique_names, normalize_name
+from ayon_server.settings.enum import folder_types_enum
+from ayon_server.types import (
+    ColorRGB_hex,
+    ColorRGBA_hex,
+    ColorRGB_float,
+    ColorRGBA_float,
+    ColorRGB_uint8,
+    ColorRGBA_uint8,
+)
+
+if TYPE_CHECKING:
+    from ayon_server.addons import BaseServerAddon
+
+
+async def async_enum_resolver():
+    """Return a list of project names."""
+    return [row["name"] async for row in Postgres.iterate("SELECT name FROM projects")]
+
+
+def enum_resolver():
+    """Return a list of value/label dicts for the enumerator.
+
+    Returning a list of dicts is used to allow for a custom label to be
+    displayed in the UI.
+    """
+    return [{"value": f"value{i}", "label": f"Label {i}"} for i in range(10)]
+
+
+async def recursive_enum_resolver(
+    addon: "BaseServerAddon",
+    project_name: str | None = None,
+    settings_variant: str = "production",
+) -> list[str]:
+    if addon is None:
+        return []
+
+    if project_name:
+        settings = await addon.get_project_settings(
+            project_name=project_name, variant=settings_variant
+        )
+    else:
+        settings = await addon.get_studio_settings(variant=settings_variant)
+
+    return settings.list_of_strings
+
+
+class Colors(BaseSettingsModel):
+    """Default is blue"""
+
+    rgb_hex: ColorRGB_hex = Field(
+        "#0000ff",
+        title="RGB Hex",
+    )
+    rgba_hex: ColorRGBA_hex = Field(
+        "#0000ff",
+        title="RGBA Hex",
+    )
+    rgb_float: ColorRGB_float = Field(
+        (0, 0, 1),
+        title="RGB Float",
+    )
+    rgba_float: ColorRGBA_float = Field(
+        (0, 0, 1, 1),
+        title="RGBA Float",
+    )
+    rgb_uint8: ColorRGB_uint8 = Field(
+        (0, 0, 255),
+        title="RGB Uint8",
+    )
+    rgba_uint8: ColorRGBA_uint8 = Field(
+        (0, 0, 255, 0),
+        title="RGBA Uint8",
+    )
+
+
+class ConditionalModel1(BaseSettingsModel):
+    _layout = "compact"
+    something: str = Field("", description="Something")
+
+
+class ConditionalModel2(BaseSettingsModel):
+    _layout = "compact"
+    something_else: str = Field("", description="Something else")
+    something_else_number: int = Field(0, description="Something else's number")
+
+
+class ConditionalModel3(BaseSettingsModel):
+    _title = "Something completely different"
+    key1: str = Field("", description="Key 1")
+    key2: str = Field("", description="Key 2")
+    key3: str = Field("", description="Key 3")
+
+
+model_switcher_enum = [
+    {"value": "model1", "label": "Something"},
+    {"value": "model2", "label": "Something else"},
+    {"value": "model3", "label": "Something completely different"},
+]
+
+
+class NestedSettings(BaseSettingsModel):
+    """Nested settings without grouping
+
+    Submodels also support docstrings, which are propagated
+    to the frontend. Just be aware a description attribute on
+    the parent field will override the docstring.
+
+    Docstring can be splitted to multiple paragraph simply
+    by adding an empty line.
+    """
+
+    spam: bool = Field(False, title="Spam")
+    eggs: bool = Field(False, title="Eggs")
+    bacon: bool = Field(False, title="Bacon")
+
+    model_switcher: str = Field(
+        "",
+        title="Model switcher",
+        description="Switch between two models",
+        enum_resolver=lambda: model_switcher_enum,
+        conditionalEnum=True,
+        section="Pseudo-dynamic models",
+    )
+
+    model1: ConditionalModel1 = Field(default_factory=ConditionalModel1)
+    model2: ConditionalModel2 = Field(default_factory=ConditionalModel2)
+    model3: ConditionalModel3 = Field(default_factory=ConditionalModel3)
+
+
+class GroupedSettings(BaseSettingsModel):
+    _isGroup = True
+    your_name: str = Field("", title="Name")
+    your_quest: str = Field("", title="Your quest")
+    favorite_color: str = Field(
+        "red",
+        title="Favorite color",
+        enum_resolver=lambda: ["red", "green", "blue"],
+    )
+
+
+class CompactListSubmodel(BaseSettingsModel):
+    # Compact layout is used, when a submodel has just a few
+    # attributes, which may be displayed in a single row
+
+    _layout = "compact"
+    name: str = Field(..., title="Name")
+    int_value: int = Field(..., title="Integer")
+    enum: list[str] = Field(
+        default_factory=list,
+        title="Enum",
+        enum_resolver=lambda: ["foo", "bar", "baz"],
+    )
+
+    @validator("name")
+    def validate_name(cls, value):
+        """Ensure name does not contain weird characters"""
+        return normalize_name(value)
+
+
+class DictLikeSubmodel(BaseSettingsModel):
+    _layout = "expanded"
+
+    name: str = Field(..., title="Name", scope=["studio", "project", "site"])
+    value1: str = Field("", title="Value 1", scope=["studio", "project", "site"])
+    value2: str = Field("", title="Value 2", scope=["studio", "project", "site"])
+    value3: str = Field("", title="Value 3", scope=["studio", "project", "site"])
+    value4: str = Field("", title="Value 4")
+
+    @validator("name")
+    def validate_name(cls, value):
+        """Ensure name does not contain weird characters"""
+        return normalize_name(value)
+
+
+class ExampleSettings(BaseSettingsModel):
+    """Test addon settings.
+
+
+    This is a test addon settings. It is used to test various
+    features of the settings system.
+
+    Docstrings are propagated to the frontend, so you can use
+    them to describe your settings, submodels and their fields.
+
+    On the frontend, docstrings are rendered as markdown, so you
+    can use markdown syntax to format your descriptions, e.g.:
+    **bold** , *italic* , `code`, [links](https://openpype.io)...
+    """
+
+    simple_string: str = Field(
+        "default value",
+        title="Simple string",
+        description="This is a simple string",
+    )
+
+    folder_type: str = Field(
+        "Asset",
+        title="Folder type",
+        description="Type of the folder the addon operates on",
+        placeholder="Select folder type",
+        enum_resolver=folder_types_enum,
+    )
+
+    textarea: str = Field(
+        "",
+        title="Textarea",
+        widget="textarea",
+        placeholder="Placeholder of the textarea field",
+    )
+
+    number: int = Field(
+        1,
+        title="Number",
+        description="Positive integer 1-10",
+        gt=0,  # greater than
+        le=10,  # less or equal
+        placeholder="Placeholder of the number field",
+    )
+
+    # Scoped fields are shown only in specific contexts (studio/project/local)
+    # By default, they are shown in studio and projects contexts
+
+    hidden_setting: str = Field(
+        "you can't see me",
+        title="Hidden setting",
+        scope=[],
+        description="This setting is hidden in all contexts",
+    )
+
+    all_scopes_setting: str = Field(
+        "You see me all the time",
+        title="All scopes",
+        scope=["studio", "project", "site"],
+        description="This setting is shown in all contexts",
+        section="Scoped fields",
+    )
+
+    studio_setting: str = Field(
+        "",
+        title="Studio setting",
+        scope=["studio"],
+        description="This setting is only visible in studio scope",
+    )
+
+    project_setting: str = Field(
+        "",
+        title="Project setting",
+        scope=["project"],
+        description="This setting is only visible in project scope",
+    )
+
+    project_site_setting: str = Field(
+        "",
+        title="Project site setting",
+        scope=["site"],
+        description="This setting is only visible in the local scope",
+    )
+
+    all_scopes_list_of_submodels: list[DictLikeSubmodel] = Field(
+        default_factory=list,
+        title="Dict-like list",
+        scope=["studio", "project", "site"],
+    )
+
+    # Simple enumerators can be defined using Literal type
+
+    simple_enum: Literal["red", "green", "blue"] = Field(
+        "red",
+        title="Simple enum",
+        section="Enumerators",
+    )
+
+    # For more complex enumerators, use enum_resolver function
+    # which returns a list of items. enum_resolver can be both
+    # async or blocking.
+
+    # section argument is used to visually separate fields of the form
+    # a horizontal line with a label will be shown just above the field
+    # with a section argument
+
+    project: str | None = Field(
+        None,
+        enum_resolver=async_enum_resolver,
+        title="Dynamic enum",
+    )
+
+    multiselect: list[str] = Field(
+        default_factory=list,
+        title="Multiselect",
+        enum_resolver=lambda: ["foo", "bar", "ba"],
+    )
+    # Enumerators can be defined using a list of dicts, where
+    # each dict has "value" and "label" keys
+
+    enum_with_labels: str = Field(
+        "value1",
+        title="Enum with labels",
+        enum_resolver=enum_resolver,
+    )
+
+    list_of_strings: list[str] = Field(
+        default_factory=list,
+        title="List of strings",
+        section="List",
+    )
+
+    recursive_enum: str = Field(
+        "",
+        title="Recursive enum",
+        enum_resolver=recursive_enum_resolver,
+        section="Pick a value from the list above",
+    )
+
+    colors: Colors = Field(
+        default_factory=Colors,
+        title="Colors",
+    )
+
+    # Settings models can be nested
+
+    nested_settings: NestedSettings = Field(
+        default_factory=NestedSettings,
+        title="Nested settings",
+    )
+
+    grouped_settings: GroupedSettings = Field(
+        default_factory=GroupedSettings,
+        title="Grouped settings",
+        description="Nested settings submodel with grouping",
+    )
+
+    list_of_submodels: list[CompactListSubmodel] = Field(
+        [
+            CompactListSubmodel(name="default", int_value=42, enum=["foo", "bar"]),
+        ],
+        title="A list of compact objects",
+        required_items=["default"],
+    )
+
+    dict_like_list: list[DictLikeSubmodel] = Field(
+        default_factory=list,
+        title="Dict-like list",
+    )
+
+    #    error: str =  Field(verybadfield)
+
+    @validator("list_of_submodels", "dict_like_list")
+    def ensure_unique_names(cls, value):
+        """Ensure name fields within the lists have unique names."""
+        ensure_unique_names(value)
+        return value
+
+
+# class ExampleSettings(BaseSettingsModel):
+#     """Test addon settings"""
+#
+#     colors: Colors = Field(default_factory=Colors, title="Colors")
